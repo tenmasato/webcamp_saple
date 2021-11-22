c38778975cd84291900858b569be8fd63ce177fb
tenmasato <narito1015@icloud.com>
2021-11-22 02:51:05 +0000

    [Add]画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e744bdd97d68551385b8d2a1c7b34bde169745873049978d4f7c18628eb9725647b5313e11edb15fcfad997eb354395de7f1e17c67d1a452f73642e00de07bd5'
